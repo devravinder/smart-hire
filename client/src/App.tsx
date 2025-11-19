055b2a876ac46c21a04e1161d96a892df122dc8b
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2025-11-19 12:22:17 +0530

    feat: auth wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
+import SupabseAuth from "./auth/SupabaseAuth.js";
 import { ThemeProvider } from "./hooks/useTheme.js";
 import Routes from "./routes/index.js";
 
 export default function App() {
   return (
     <ThemeProvider>
-        <Routes />
+      <SupabseAuth/>
+        {/* <Routes /> */}
     </ThemeProvider>
   );
 }
